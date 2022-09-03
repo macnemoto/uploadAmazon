836159d23926835a591ab2e89801ca9480b3f0fb
macnemoto <marcosj465@gmail.com>
2022-09-03 02:33:52 -0400

    little changes :hammer:

--- routes/routesUpload.js
+++ routes/routesUpload.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const upload = require("../upload");
 
 router.post("/image-upload", upload.single("image"), function (req, res, next) {
-  return res.json({ imageUrl: req.file.location, titutlo: req.body.title });
+  return res.json({
+    imageUrl: req.file.location,
+    titutlo: req.body.title,
+    descripcion: req.body.description,
+    precio: req.body.price
+    });
 });
 
 module.exports = router;
