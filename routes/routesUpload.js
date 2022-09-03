c296d6b4c59b46961bda9a8e48719840de5b5b87
macnemoto <marcosj465@gmail.com>
2022-09-03 01:45:49 -0400

    adding new dependencies amazon aws v3 and multer 3 :heavy_plus_sign:

--- routes/routesUpload.js
+++ routes/routesUpload.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const upload = require('../upload');
+const upload = require("../upload");
 
-const singleUpload = upload.single('image')
-
-router.post('/image-upload', function(req, res) {
-  singleUpload(req, res, function(err, some) {
-    if (err) {
-      return res.status(422).send({errors: [{title: 'Image Upload Error', detail: err.message}] });
-    }
-
-    return res.json({'imageUrl': req.file.location, 'titutlo': req.title});
-  });
-})
+router.post("/image-upload", upload.single("image"), function (req, res, next) {
+  return res.json({ imageUrl: req.file.location, titutlo: req.body.title });
+});
 
 module.exports = router;
--- upload.js
+++ upload.js
@@ -1,30 +1,26 @@
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3-v2');
-require('dotenv').config()
+require("dotenv").config();
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 
-aws.config.update({
-    secretAccessKey: process.env.SECRET_KEY,
-    accessKeyId: process.env.ACC_KEY,
-    region: REGION
+const s3 = new S3Client({
+  region: process.env.REGION,
+  accessKeyId: process.env.ACC_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
 });
 
-const s3 = new aws.S3();
-
-
 const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: 'cultalaimagen',
-      ContentType: 'image/png',
-       acl: 'public-read',
-      metadata: function (req, file, cb) {
-        cb(null,);
-      },
-      key: function (req, file, cb) {
-        cb(null,`${file.originalname}`)
-      }
-    })
-  })
-  //Date.now() + '-' +  
-  module.exports = upload;
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+module.exports = upload;
