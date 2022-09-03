81d6467653f8170089613e5330b302f3aa9853aa
macnemoto <marcosj465@gmail.com>
2022-09-03 10:05:07 -0400

    minimal changes :hammer:

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-//Escuchando
+//server escuchado puerto 3000
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-
-
-app.use('/', require('./routes/routesUpload'));
-
+app.use("/", require("./routes/routesUpload"));
--- upload.js
+++ upload.js
@@ -1,30 +1,29 @@
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3-v2');
-require('dotenv').config()
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3-v2");
+require("dotenv").config();
 
 aws.config.update({
-    secretAccessKey: process.env.SECRET_KEY,
-    accessKeyId: process.env.ACC_KEY,
-    region: REGION
+  secretAccessKey: process.env.SECRET_KEY,
+  accessKeyId: process.env.ACC_KEY,
+  region: REGION,
 });
 
 const s3 = new aws.S3();
 
-
 const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: 'cultalaimagen',
-      ContentType: 'image/png',
-       acl: 'public-read',
-      metadata: function (req, file, cb) {
-        cb(null,);
-      },
-      key: function (req, file, cb) {
-        cb(null,`${file.originalname}`)
-      }
-    })
-  })
-  //Date.now() + '-' +  
-  module.exports = upload;
+  storage: multerS3({
+    s3: s3,
+    bucket: "cultalaimagen",
+    ContentType: "image/png",
+    acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null);
+    },
+    key: function (req, file, cb) {
+      cb(null, `${file.originalname}`);
+    },
+  }),
+});
+//Date.now() + '-' +
+module.exports = upload;
